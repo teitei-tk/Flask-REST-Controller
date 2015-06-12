4f18e2a57c342ea87b501ea9a1e9399891f84f5f
teitei-tk <teitei.tk@gmail.com>
2015-06-12 22:12:54 +0900

    modify: change import module logic, use werkzeug.import_string

--- flask_rest_controller/routing.py
+++ flask_rest_controller/routing.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 
+from werkzeug import import_string
 from collections import namedtuple
 
-__all__ = ['Router', 'convert_routing_module', 'import_module_by_string', 'set_routing']
+__all__ = ['Router', 'convert_routing_module', 'set_routing']
 
 # add router class
 Router = namedtuple('Router', ['url', 'import_path', 'endpoint'])
@@ -10,13 +11,6 @@ Router = namedtuple('Router', ['url', 'import_path', 'endpoint'])
 
 def convert_routing_module(view_datas):
     return [Router(url=data[0], import_path=data[1], endpoint=data[2]) for data in view_datas]
-
-
-def import_module_by_string(name):
-    module_name, object_name = name.rsplit('.', 1)
-
-    module = __import__(module_name)
-    return getattr(module, object_name)
 
 
 def set_routing(app, view_data):
@@ -36,5 +30,5 @@ def set_routing(app, view_data):
     routing_modules = convert_routing_module(view_data)
 
     for module in routing_modules:
-        view = import_module_by_string(module.import_path)
+        view = import_string(module.import_path)
         app.add_url_rule(module.url, view_func=view.as_view(module.endpoint))
